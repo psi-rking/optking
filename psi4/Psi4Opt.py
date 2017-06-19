0af87e037dc1efdada8068fe10647b04b597a641
Rollin <rking@bethel.edu>
2017-06-19 15:14:10 -0500

    added 2 test cases; simplified psi4 input tests with Psi4Opt.py

--- psi4/Psi4Opt.py
+++ psi4/Psi4Opt.py
@@ -1,28 +1,12 @@
+import psi4
+from psi4 import *
+from psi4.core import *
 import numpy as np
 
-molecule hooh {
-    H            1.699924772228     1.549001852664     0.729368159665
-    O           -0.027495833355     1.120334367050     0.682522182417
-    O           -0.047683750414    -1.071778830756    -0.755485307218
-    H           -0.506770221333    -2.319613449532     0.428609578964
- unit au
-}
-
-set {
-  basis dz
-}
-
-set optking {
-  Geom_maxiter  1
-  step_type     rfo
-  hess_update   bfgs
-  intrafrag_hess SIMPLE
-}
-
-# Create initial molecular structure object in optking
-#  (atomic numbers, cartesians, masses, ...) using class method
-#  in optking.frag.py for a PSI4 molecule.
+# Create initial molecular structure object in optking (atomic numbers,
+# cartesian coordinates, masses, ...) using class method for PSI4 molecule.
 mol = core.get_active_molecule()
+calcName = 0
 import optking
 Molsys = optking.molsys.MOLSYS.fromPsi4Molecule(mol)
 
@@ -33,7 +17,7 @@ optking_user_options = {}
 for opt,optval in optking_options.items():
     if optval['has_changed'] == True:
         optking_user_options[opt] = optval['value']
-
+    
 # Define a function for optking that takes a Cartesian, numpy geometry, and
 #  puts it in place for subsequent gradient computations.  This function
 #  may move COM or reorient; changes argument to match such geometry.
@@ -46,7 +30,7 @@ def setGeometry_func( newGeom ):
 # Define a function for optking that returns an energy and cartesian
 #  gradient in numpy array format.
 def gradient_func(printResults=True):
-    psi4gradientMatrix, wfn = driver.gradient('hf', molecule=mol, return_wfn=True)
+    psi4gradientMatrix, wfn = driver.gradient(calcName, molecule=mol, return_wfn=True)
     gradientMatrix = np.array( psi4gradientMatrix )
     E = wfn.energy()
     if printResults:
@@ -56,7 +40,7 @@ def gradient_func(printResults=True):
     return E, np.reshape(gradientMatrix, (gradientMatrix.size))
 
 def hessian_func(printResults=True):
-    H = driver.hessian('hf', molecule=mol)
+    H = driver.hessian(calcName, molecule=mol)
     if printResults:
         print '\t Hessian'
         print H
@@ -65,20 +49,14 @@ def hessian_func(printResults=True):
 #  This function only matters for special purposes like 1D line searching.
 # The energy is usually obtained from the gradient function.
 def energy_func(printResults=True):
-    E, wfn = driver.energy('hf', molecule=mol, return_wfn=True)
+    E, wfn = driver.energy(calcName, molecule=mol, return_wfn=True)
     if printResults:
         print '\tEnergy: %15.10f' % E
     return E
 
-# Provide optimizer function the following arguments:
-# 1. optking.molsys.MOLSYS system (defined by class method above.
-# 2. a dictionary of optking options
-# 3. a method which prepares a given Cartesian, numpy geometry
-#     for subsequent gradient computation or analysis.
-# 4. a method to compute the gradient, which returns the energy and gradient.
-# 5. optional hessian function
-# 6. optional energy function for line-searching algorithms
+def Psi4Opt():
+    thisenergy = optking.optimize( Molsys, optking_user_options, setGeometry_func, gradient_func, \
+                    hessian_func, energy_func )
+    return thisenergy
 
-optking.optimize( Molsys, optking_user_options, setGeometry_func, gradient_func, \
-                hessian_func, energy_func )
 
