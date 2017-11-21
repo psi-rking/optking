4bf2ac30670a41fd0e6321a5d1666fca16ce101c
Daniel Smith <malorian@me.com>
2017-11-21 15:32:17 -0500

    Starts a Psi4 test file, skipping for now

---
+++ tests/psi4_helper.py
@@ -0,0 +1,16 @@
+"""
+A simple OptKing wrapper for the Psi4 quantum chemistry program
+"""
+
+import numpy
+import pytest
+
+# Try to pull in Psi4
+try:
+    import psi4
+    found_psi4 = True
+except ImportError:
+    found_psi4 = False
+
+# Wrap Psi4 in ifden
+using_psi4 = pytest.mark.skipif(found_psi4, reason="Psi4 not found, skipping.")
