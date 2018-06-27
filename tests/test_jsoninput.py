f160720bd13341141a437eb2830272917c7d9a3c
Alexander Heide <alh39973@bethel.edu>
2018-06-27 14:53:57 -0400

    still trying to fix jsoninput.json for travis

--- tests/test_jsoninput.py
+++ tests/test_jsoninput.py
@@ -1,11 +1,12 @@
 import runoptkingjson
 import psi4
+import os
 
 refnucenergy = 8.9064890670
 refenergy = -74.965901192 
 
 def test_input_through_json():
-    energy, nucenergy = runoptkingjson.run_optking_json('../tests/jsoninput.json')
+    energy, nucenergy = runoptkingjson.run_optking_json(os.path.join(os.path.dirname(__file__), 'jsoninput.json'))
 
     assert psi4.compare_values(refnucenergy, nucenergy, 3, "Nuclear repulsion energy")
     assert psi4.compare_values(refenergy, energy, 6, "Reference energy") 
