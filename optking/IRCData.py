6799484e85a49155c23350267f7fe1d09273a33d
Rollin King <rking@bethel.edu>
2018-12-04 09:55:22 -0600

    Added IRCData.py structure to contain the IRC path information. Its progress_report() should be modified to point to our logger.

--- optking/IRCData.py
+++ optking/IRCData.py
@@ -1,10 +1,11 @@
 ### Class to store points on the IRC
+import logging
 
 class IRCpoint(object):
     """Holds data for one step on the IRC.
     Parameters
     ----------
-    coord_step : int
+    step_number : int
         step number
     q_pivot :    ndarray
         pivot point for step
@@ -25,8 +26,8 @@ class IRCpoint(object):
     line_dist :  float
     """
 
-    def __init__(self, coord_step, q_pivot, x_pivot, q, x, f_q, f_x, energy, step_dist, arc_dist, line_dist):
-        self.coord_step = coord_step
+    def __init__(self, step_number, q_pivot, x_pivot, q, x, f_q, f_x, energy, step_dist, arc_dist, line_dist):
+        self.step_number = step_number
         self.q_pivot = q_pivot
         self.x_pivot = x_pivot
         self.q = q
@@ -45,19 +46,30 @@ class IRCdata(object):
     def __init__(self):
         self.go = True
         self.in_min_range = False
-        self.step_length = 0.4 #fill with parameter value 
-
+        self.step_length = 0.4 # TODO: fill with parameter value 
         self.sphere_step = 0
-        #self.coord_step  needed?
-        self.step_dist = 0.0
-        self.arc_dist = 0.0
-        self.line_dist = 0.0
-        self.arc_length = 0.0
-        self.line_length = 0.0
+        self.running_arc_dist = 0.0
+        self.running_line_dist = 0.0
+        # If not too inconvenient, require as arguments in add_irc_point()
+        self.arc_length = 0.0  # temporary storage for current step length
+        self.line_length = 0.0  # temporary storage for current step length
 
         self.irc_points = []
 
+    def add_irc_point(self, step_number, q_p, x_p, q, x, f_q, f_x, E):
+        step_dist = step_number * self.step_length
+        arc_dist  = self.running_arc_dist + self.arc_length # optionally computed
+        line_dist = self.running_line_dist + self.line_length # optionally computed
+
+        onepoint = IRCpoint(step_number, q_p, x_p, q, x, f_q, f_x, E,
+                            step_dist, arc_dist, line_dist)
+        self.irc_points.append(onepoint)
+
     # Return most recent IRC step data unless otherwise specified
+    def step_number(self, step=None):
+        index = -1 if step is None else step
+        return self.irc_points[index].step_number
+
     def q_pivot(self, step=None):
         index = -1 if step is None else step
         return self.irc_points[index].q_pivot
@@ -66,13 +78,13 @@ class IRCdata(object):
         index = -1 if step is None else step
         return self.irc_points[index].x_pivot
 
-    def g_q(self, step=None):
+    def q(self, step=None):
         index = -1 if step is None else step
-        return self.irc_points[index].g_q
+        return self.irc_points[index].q
 
-    def g_x(self, step=None):
+    def x(self, step=None):
         index = -1 if step is None else step
-        return self.irc_points[index].q_x
+        return self.irc_points[index].x
 
     def f_q(self, step=None):
         index = -1 if step is None else step
@@ -82,114 +94,109 @@ class IRCdata(object):
         index = -1 if step is None else step
         return self.irc_points[index].f_x
 
+    def energy(self, step=None):
+        index = -1 if step is None else step
+        return self.irc_points[index].energy
+
     def line_dist(self, step=None):
         index = -1 if step is None else step
         return self.irc_points[index].line_dist
 
-    def add_irc_point(self, coord_step, q_p, x_p, q, x, f_q, f_x, E):
-        self.step_dist  = coord_step * self.step_length
-        self.arc_dist  += self.arc_length
-        self.line_dist += self.line_length
+    def arc_dist(self, step=None):
+        index = -1 if step is None else step
+        return self.irc_points[index].arc_dist
 
-        onepoint = IRCpoint(coord_step, q_p, x_p, q, x, f_q, f_x, E,
-                             self.step_dist, self.arc_dist, self.line_dist)
-        self.irc_points.append(onepoint)
+    def step_dist(self, step=None):
+        index = -1 if step is None else step
+        return self.irc_points[index].step_dist
 
-"""
+    def set_arc_length(self, arc_length_in, step=None):
+        index = -1 if step is None else step
+        self.irc_points[index].arc_length = arc_length_in 
+
+    def set_line_length(self, line_length_in, step=None):
+        index = -1 if step is None else step
+        self.irc_points[index].line_length = line_length_in 
+
     def progress_report(self):
+        blocks = 4 # TODO: make dynamic
+        sign = 1
+        # TODO: if OptParams.IRC_direction == 'backward': sign = -1
+        Ncoord = len(self.q())
 
-  double DE;
-  int dim = mol.Ncoord();
-  int blocks = 4;
-  int sign = 1;
+        logging.basicConfig(filename='ircprogress.log',level=logging.DEBUG)
 
-  if(Opt_params.IRC_direction == OPT_PARAMS::BACKWARD)
-    sign = -1;
+        out = '\n' #string for output
 
-//Printing Energies and Energy Changes for Each Step
-  oprintf_out("@IRC ----------------------------------------------\n");
-  oprintf_out("@IRC            ****      IRC Report      ****\n");
-  oprintf_out("@IRC ----------------------------------------------\n");
-  oprintf_out("@IRC  Step    Energy              Change in Energy \n");
-  oprintf_out("@IRC ----------------------------------------------\n");
-  for (std::size_t i=0; i<steps.size(); ++i)
-  {
-    if (i == 0) DE = g_step(i).g_energy();
-    else DE = g_step(i).g_energy() - g_step(i-1).g_energy();
+        #Print Energies and Energy Changes for Each Step
+        out += "@IRC ----------------------------------------------\n"
+        out += "@IRC            ****      IRC Report      ****\n"
+        out += "@IRC ----------------------------------------------\n"
+        out += "@IRC  Step    Energy              Change in Energy \n"
+        out += "@IRC ----------------------------------------------\n"
+        for i in range(len(self.irc_points)):
+            if i == 0:
+                DE = self.energy(i)
+            else:
+                DE = self.energy(i) - self.energy(i-1)
+            out += "@IRC  %3d %18.12lf  %18.12lf\n" % (i, self.energy(i), DE)
+        out += "@IRC ----------------------------------------------\n\n"
 
-    oprintf_out("@IRC  %3d %18.12lf  %18.12lf\n", i, g_step(i).g_energy(), DE);
-  }
-  oprintf_out("@IRC ----------------------------------------------\n\n");
+        # Print Internal Coordinates for Each step
+        out += "@IRC -----------------------------------------------------\n"
+        out += "@IRC              ****     IRC Steps     ****             \n"
+        out += "@IRC -----------------------------------------------------"
+        for j in range(Ncoord//blocks):
+            out += "\n@IRC        |          Distance         |\n"
+            out +=   "@IRC Step   | Step    Arc       Line    |"
+            for i in range(j*blocks, (j+1)* blocks):
+                out += "    Coord %3d" % i
+            out += "\n"
+            out += "@IRC --------------------------------------"
+            for i in range(j*blocks, (j+1)* blocks):
+                out += "-------------"
+            out += "\n"
+            for i in range(len(self.irc_points)):
+                out += "@IRC  %3d %9.2lf %9.5lf  %9.5lf   " % (i, sign*self.step_dist(i),
+                    sign*self.arc_dist(i), sign*self.line_dist(i))
+                for k in range(j*blocks, (j+1)*blocks):
+                    out += "%13.8f" % self.q(i)[k]
+                out += "\n"
+          
+            out += "@IRC --------------------------------------"
+            for i in range(j*blocks, (j+1)* blocks):
+                out += "-------------"
+        if Ncoord % blocks != 0:
+            out += "\n@IRC         |          Distance         |\n"
+            out +=   "@IRC  Step   | Step    Arc       Line    |"
 
-//Printing Internal Coordinates for Each step
-  oprintf_out("@IRC -----------------------------------------------------\n");
-  oprintf_out("@IRC              ****     IRC Steps     ****             \n");
-  oprintf_out("@IRC -----------------------------------------------------");
-  for(int j=0; j < dim/blocks; j++)
-  {
-    oprintf_out("\n@IRC        |          Distance         |\n");
-    oprintf_out(  "@IRC Step   | Step    Arc       Line    |");
-    for(int i = (j*blocks); i < ((j+1)* blocks); i++)
-    {
-      oprintf_out("    Coord %3d", i);
-    }
-    oprintf_out("\n");
-    oprintf_out("@IRC --------------------------------------");
-    for(int i = (j*blocks); i < ((j+1)* blocks); i++)
-    {
-      oprintf_out("-------------");
-    }
-    oprintf_out("\n");
-    for (std::size_t i=0; i<steps.size(); ++i)
-    {
-      oprintf_out("@IRC  %3d %9.2lf %9.5lf  %9.5lf   ", i, sign*g_step(i).g_step_dist(), sign*g_step(i).g_arc_dist(), sign*g_step(i).g_line_dist());
-      for(int k = (j*blocks); k < ((j+1)*blocks); k++)
-        oprintf_out("%13.8f",g_step(i).g_q()[k]);
-      oprintf_out("\n");
+            for i in range(Ncoord - (Ncoord % blocks), Ncoord):
+                out += "    Coord %3d" % i
+            out += "\n"
+            out += "@IRC --------------------------------------"
 
-        oprintf_out("%13.8f",g_step(i).g_q()[k]);
-      oprintf_out("\n");
-    }
-    oprintf_out("@IRC --------------------------------------");
-    for(int i = (j*blocks); i < ((j+1)* blocks); i++)
-    {
-      oprintf_out("-------------");
-    }
-  }
-  if(dim % blocks != 0)
-  {
-    oprintf_out("\n@IRC         |          Distance         |\n");
-    oprintf_out(  "@IRC  Step   | Step    Arc       Line    |");
-    for(int i = (dim - (dim % blocks)); i < dim; i++)
-    {
-      oprintf_out("    Coord %3d", i);
-    }
-    oprintf_out("\n");
-    oprintf_out("@IRC --------------------------------------");
-    for(int i = (dim - (dim % blocks)); i < dim; i++)
-    {
-      oprintf_out("-------------");
-    }
-    oprintf_out("\n");
-    for (std::size_t i=0; i<steps.size(); ++i)
-    {
-      oprintf_out("@IRC  %3d %9.2lf %9.5lf  %9.5lf   ", i, sign*g_step(i).g_step_dist(), sign*g_step(i).g_arc_dist(), sign*g_step(i).g_line_dist());
-      for(int k = (dim - (dim % blocks)); k < dim; k++)
-        oprintf_out("%13.8f",g_step(i).g_q()[k]);
-      oprintf_out("\n");
-    }
-    oprintf_out("@IRC --------------------------------------");
-    for(int i = (dim - (dim % blocks)); i < dim; i++)
-    {
-      oprintf_out("-------------");
-    }
-  }
+            for i in range(Ncoord - (Ncoord % blocks), Ncoord):
+                out += "-------------"
+            out += "\n"
 
-  oprintf_out("\n");
-  oprintf_out("\n");
+            for i in range(len(self.irc_points)):
+                out += "@IRC  %3d %9.2lf %9.5lf  %9.5lf   " % (i,
+                    sign*self.step_dist(i), sign*self.arc_dist(i), sign*self.line_dist(i))
+                for k in range(Ncoord - (Ncoord % blocks), Ncoord):
+                    out += "%13.8f" % self.q(i)[k]
+                out += "\n"
 
-  mol.print_coords(psi_outfile, qc_outfile);
-  mol.print_simples(psi_outfile, qc_outfile);
-}
-"""
+            out += "@IRC --------------------------------------"
+
+            for i in range(Ncoord - (Ncoord % blocks), Ncoord):
+                out += "-------------"
+        
+        out += "\n"
+        out += "\n"
+
+        #print(out,end="")
+        logging.info(out)
+
+        #out += mol.print_coords(psi_outfile, qc_outfile)
+        #out += mol.print_simples(psi_outfile, qc_outfile)
 
