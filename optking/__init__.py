a1ec013bba2c7448ace57dee3f9e75c913218aea
Rollin King <rking@bethel.edu>
2019-06-17 18:33:37 -0500

    testing order of modules in init

--- optking/__init__.py
+++ optking/__init__.py
@@ -3,7 +3,6 @@ import logging
 import os
 import sys
 
-from .optimize import optimize
 from . import lj_functions
 from . import loggingconfig
 from .psi4optwrapper import Psi4Opt
@@ -19,6 +18,7 @@ from .qcdbjson import jsonSchema
 from .displace import displace
 from . import optparams as op
 from .exceptions import OptError, AlgError
+from .optimize import optimize
 
 # this was on my TODO list, but now im not sure its working
 op.Params = op.OptParams({})
