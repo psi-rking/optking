Correct reading hessians from disk / compute. Fix bug where full_hess_every guessed hessian

optking/hessian.py
@@ -89,15 +89,18 @@ def from_file(filename):
         if '.json' == filename[:-5]:
             result = json.load(f)
             hess = result['return_result']
+            natom = len(result['molecule']['symbols'])
         else:
             # 2D split. Cast everything to floats and convert back to 2D list from map.
             lines = f.readlines()
-            hess = [list(map(float, line.split())) for line in lines]
-
+            hess = [list(map(float, line.split())) for line in lines[1:]]
+            natom = int(lines[0].split()[1])
     try:
-        H = np.fromiter(hess, dtype=float)
+        H = np.array(hess, dtype=float)
+        H = H.reshape(natom, natom)
     except (IndexError, ValueError, TypeError) as error:
         logger.error("Hessian should be 3Nx3N cartesian force constant matrix or provided by MolSSI Schema")
+        logger.error(error)
         raise ValueError("Could not load hessian from disk") from error
     else:
         return H

optking/molsys.py
@@ -720,7 +720,6 @@ class Molsys(object):
             Ginv = symm_mat_inv(G, redundant=True)
             g_q = coeff * np.dot(np.dot(Ginv, B), g_x)
 
-        logger.info("B matrix \n\t%s", B)
         return g_q
 
     def hessian_to_internals(self, H, g_x=None, useMasses=False):
@@ -744,7 +743,7 @@ class Molsys(object):
         Hq : np.ndarray
             hessian in internal coordinates
         """
-        logger.info("Converting Hessian from cartesians to internals.\n")
+        logger.info("Converting Hessian from cartesians to internals.")
 
         B = self.Bmat()
 
@@ -760,7 +759,7 @@ class Molsys(object):
 
         Hworking = H.copy()
         if g_x is None:  # A^t Hxy A
-            logger.info("Neglecting force/B-matrix derivative term, only correct at" + "stationary points.\n")
+            logger.info("Neglecting force/B-matrix derivative term, only correct at" + "stationary points.")
         else:  # A^t (Hxy - Kxy) A;    K_xy = sum_q ( grad_q[I] d^2(q_I)/(dx dy) )
             logger.info("Including force/B-matrix derivative term.\n")
 

optking/opt_helper.py
@@ -156,7 +156,7 @@ class Helper(ABC):
             logger.info(str(self.molsys))
 
         self._compute()
-        logger.info("%s", print_geom_grad(self.geom, self.gX))
+        logger.info("\n\t%s", print_geom_grad(self.geom, self.gX))
         self.fq = self.molsys.gradient_to_internals(self.gX, -1.0)
 
         self.molsys.apply_external_forces(self.fq, self._Hq, self.step_num)
@@ -202,7 +202,7 @@ class Helper(ABC):
                 self._gX = val
             else:
                 if val.shape == (self.molsys.natom, 3):
-                    self._gX = val.reshape(1, ncart)
+                    self._gX = np.ravel(val)
                 else:
                     raise TypeError(
                         f"Gradient must an iterable with shape (3, {self.molsys.natom}) or (1, {ncart})"
@@ -338,21 +338,23 @@ class CustomHelper(Helper):
         if self.HX is None:
             if "hessian" in self.calculations_needed():
                 if self.params.cart_hess_read:
-                    self.HX = hessian.from_file(self.params.hessian_file)  # set ourselves if file
-                    result = self.computer.compute(self.geom, driver="hessian")
-                    self.gX = result["extras"]["qcvars"]["CURRENT GRADIENT"]
+                    self.HX = hessian.from_file(self.params.hessian_file)  # set ourselves if file\
+                    _ = self.computer.compute(self.geom, driver="hessian")
                     self._Hq = self.molsys.hessian_to_internals(self.HX)
                     self.HX = None
+                    self.params.cart_hess_read = False
+                    self.params.hessian_file = None
                 else:
                     raise RuntimeError("Optking requested a hessian but was not provided one. "
                                        "This could be a driver issue")
-            if self.step_num == 0:
-                logger.info("Guessing hessian")
+            elif self.step_num == 0:
+                logger.debug("Guessing hessian")
                 self._Hq = hessian.guess(self.molsys, guessType=self.params.intrafrag_hess)
+                self.gX = self.computer.compute(self.geom, driver="gradient", return_full=False)
             else:
-                logger.info("Updating hessian")
+                logger.debug("Updating hessian")
                 self._Hq = self.history.hessian_update(self._Hq, self.molsys)
-            self.gX = self.computer.compute(self.geom, driver="gradient", return_full=False)
+                self.gX = self.computer.compute(self.geom, driver="gradient", return_full=False)
         else:
             result = self.computer.compute(self.geom, driver="hessian")
             self.HX = result["return_result"]

optking/optimize.py
@@ -120,7 +120,7 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
 
         requirements = self.opt_method.requires()
         protocol = self.get_hessian_protocol()
-        H, gX, E = get_pes_info(H, self.computer, self.molsys, self.history, protocol, requirements)
+        H, gX, E = get_pes_info(H, self.computer, self.molsys, self.history, self.params, protocol, requirements)
 
         logger.info("%s", print_geom_grad(self.molsys.geom, gX))
 
@@ -168,7 +168,6 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
                 self.check_linesearch = True
 
         self.requires = self.update_requirements()
-        logger.info(achieved_dq)
         if return_str:
             return achieved_dq, returned_str
         return achieved_dq
@@ -188,7 +187,7 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
             if str_mode:
                 return converged
             if converged is True:
-                logger.info("\tConverged in %d steps!" % self.step_number)
+                logger.info("\tConverged in %d steps!" % step_number)
                 logger.info("\tFinal energy is %20.13f" % E)
                 logger.info("\tFinal structure (Angstroms): \n" + self.molsys.show_geom())
         return converged
@@ -225,6 +224,9 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
             self.erase_hessian = False
             return "guess"
 
+        if self.params.cart_hess_read:
+            return "compute"
+
         if self.step_number <= 1:
             if self.params.opt_type != "IRC":
                 if self.params.full_hess_every > -1:  # compute hessian at least once.
@@ -237,7 +239,7 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
             if self.params.full_hess_every < 1:
                 protocol = "update"
             elif self.step_number % self.params.full_hess_every == 0:
-                protocol = "update"
+                protocol = "compute"
             else:
                 protocol = "update"
 
@@ -351,9 +353,9 @@ def get_pes_info(H,
                  computer,
                  o_molsys,
                  opt_history,
+                 params,
                  hessian_protocol="update",
                  requires=("energy", "gradient"),
-                 hessian_file=None
                  ):
     """Calculate, update, or guess hessian as appropriate. Calculate gradient, pulling
     gradient from hessian output if possible.
@@ -379,7 +381,10 @@ def get_pes_info(H,
     else:
         driver = "energy"
 
-    if hessian_protocol == "compute" and hessian_file is None:
+    self.logger.info(f"hessian protocol {hessian_protocol}")
+    self.logger.info(f"hessian file {hessian_file}")
+
+    if hessian_protocol == "compute" and not params.cart_hess_read:
         H, g_X = get_hess_grad(computer, o_molsys)
     elif hessian_protocol == "update":
         logger.info(f"Updating Hessian with {str(op.Params.hess_update)}")
@@ -394,8 +399,10 @@ def get_pes_info(H,
     elif hessian_protocol == "unneeded":
         result = computer.compute(o_molsys.geom, driver=driver, return_full=False)
         g_X = np.asarray(result) if driver == "gradient" else None
-    elif hessian_file:
+    elif params.cart_hess_read:
         H = hessian.from_file(hessian_file)
+        params.cart_hess_read = False
+        params.hessian_file = None
 
     else:
         raise OptError("Encountered unknown value from get_hessian_protocol()")