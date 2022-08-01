c6a66a1d6d9c43a634ae48abb19f7422e5da97f9
AlexHeide <agh66737@uga.edu>
2022-08-01 13:52:10 -0400

    Add missing test utils

---
+++ optking/tests/utils/utils.py
@@ -0,0 +1,14 @@
+import optking
+
+
+def compare_iterations(json_output, expected_steps, assert_iter):
+
+    logger = optking.logger
+    steps_taken = len(json_output['trajectory'])
+    if steps_taken != expected_steps:
+        logger.warning(f"TEST - Number of steps taken {steps_taken}, Previous required steps {expected_steps}")
+    else:
+        logger.info(f"TEST - Number of steps taken matches expected {steps_taken}")
+
+    if int(assert_iter):
+        assert steps_taken == expected_steps
