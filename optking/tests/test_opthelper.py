41948ca2a796f78effebd80aa52ec06a585aedb6
Alexander Heide <alexander.heide@uga.edu>
2022-09-22 15:56:46 -0400

    Add CustomHelper liensearch test

--- optking/tests/test_opthelper.py
+++ optking/tests/test_opthelper.py
@@ -203,7 +203,6 @@ def test_hooh_irc(check_iter):
 
     for i in range(50):
         opt = optking.CustomHelper.from_dict(optSaved)
-        print(opt.step_num)
 
         if i == 0:
             H = psi4.hessian("hf")
@@ -212,10 +211,8 @@ def test_hooh_irc(check_iter):
         grad, wfn = psi4.gradient("hf", return_wfn=True)
         opt.gX = grad.np.reshape(-1)
         opt.E = wfn.energy()
-        print(opt.pre_step_str())
         opt.compute()
         opt.take_step()
-        print(opt.post_step_str())
         conv = opt.test_convergence()
         if conv is True:
             print("Optimization SUCCESS:")
@@ -235,3 +232,63 @@ def test_hooh_irc(check_iter):
 
     assert psi4.compare_values(energy_5th_IRC_pt, IRC[5]["energy"], 6, "Energy of 5th IRC point.")  # TEST
     utils.compare_iterations(json_output, 45, check_iter)
+
+
+def test_linesearch(check_iter):
+    import psi4
+    from .utils import utils
+    
+    refenergy = -1053.880393  # Eh
+    Ar2 = psi4.geometry(
+        """
+      Ar
+      Ar 1 5.0
+    """
+    )
+
+    psi4.core.clean_options()
+    psi4_options = {
+        "basis": "cc-pvdz",
+        "d_convergence": 10
+    }
+
+    optking_options = {
+        "geom_maxiter": 60,
+        "g_convergence": "gau_tight",
+        "step_type": "SD",
+    }
+
+    psi4.set_options(psi4_options)
+    opt = optking.CustomHelper(Ar2, optking_options)
+    optSaved = opt.to_dict()
+
+    for _ in range(50):
+        opt = optking.CustomHelper.from_dict(optSaved)
+
+        grad, wfn = psi4.gradient("mp2", return_wfn=True)
+        opt.gX = grad.np.reshape(-1)
+        opt.E = wfn.energy()
+        opt.compute()
+        opt.take_step()
+        conv = opt.test_convergence()
+        if conv is True:
+            print("Optimization SUCCESS:")
+            break
+        else:
+            optSaved = opt.to_dict()
+        geom = psi4.core.Matrix.from_array(opt.molsys.geom)
+        Ar2.set_geometry(geom)
+        Ar2.update_geometry()
+
+    else:
+        print("Optimization FAILURE:\n")
+
+    psi4.set_options(psi4_options)
+
+    # "linesearch" is not currrently recognized by psi4 read_options.
+    json_output = opt.close()
+    E = json_output["energies"][-1]
+    nucenergy = json_output["trajectory"][-1]["properties"]["nuclear_repulsion_energy"]
+    assert psi4.compare_values(nucenergy, nucenergy, 3, "Nuclear repulsion energy")  # TEST
+    assert psi4.compare_values(refenergy, E, 1, "Reference energy")  # TEST
+    utils.compare_iterations(json_output, 25, check_iter)
