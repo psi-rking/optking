IRC bug fixes. Steps not being added to history, incorrect massweighting of first step, incorrect sign when scaling pivot point step, incorrect massweighting during hypersphere opt, allows linear_bend error handling by force updating IRC history, adds step length check from old c++ optking

optking/IRCdata.py
@@ -1,4 +1,3 @@
-""" Class to store points on the IRC """
 import logging
 import os
 import copy
@@ -6,7 +5,7 @@ import copy
 import numpy as np
 
 from .exceptions import OptError
-from .printTools import print_geom_string, print_array_string
+from .printTools import print_geom_string
 from .linearAlgebra import symm_mat_inv
 from . import log_name
 from . import addIntcos
@@ -106,7 +105,7 @@ class IRCHistory(object):
 
     def __init__(self):
         self.go = True
-        self.irc_points = []
+        self.irc_points: [IRCpoint] = []
         self.atom_symbols = None
 
     def set_atom_symbols(self, atom_symbols):  # just for printing
@@ -117,7 +116,6 @@ class IRCHistory(object):
         self.__direction = direction
 
     def to_dict(self):
-
         d = {
             "irc_points": [point.to_dict() for point in self.irc_points],
             "go": self.go,
@@ -129,7 +127,6 @@ class IRCHistory(object):
 
     @classmethod
     def from_dict(cls, d):
-
         irc_history = cls()
         irc_history.irc_points = [IRCpoint.from_dict(point) for point in d["irc_points"]]
         irc_history.go = d["go"]
@@ -179,7 +176,10 @@ class IRCHistory(object):
     def add_pivot_point(self, q_p, x_p, step=None):
         index = -1 if step is None else step
         pindex = (len(self.irc_points) - 1) if step is None else step
-        logger.debug("Adding pivot point (index %d) for finding rxnpath point %d" % (pindex, pindex + 1))
+        logger.debug(
+            "Adding pivot point (index %d) for finding rxnpath point %d"
+            % (pindex, pindex + 1)
+        )
         self.irc_points[index].add_pivot(q_p, x_p)
 
     # Return most recent IRC step data unless otherwise specified
@@ -235,12 +235,9 @@ class IRCHistory(object):
         return self.irc_points[index].step_dist
 
     def test_for_dissociation(
-            self,
-            new_molsys: molsys.Molsys,
-            old_molsys: molsys.Molsys,
-            threshold=0.4
-        ):
-        """ Check whether or not the connectivity has changed and multiple fragments have
+        self, new_molsys: molsys.Molsys, old_molsys: molsys.Molsys, threshold=0.4
+    ):
+        """Check whether or not the connectivity has changed and multiple fragments have
         been created. This may be used to terminate the IRC. This method should only be
         called if frag_mode == 'SINGLE'.
 
@@ -288,8 +285,6 @@ class IRCHistory(object):
         Two checks are performed.
         1. If forces are opposite those are previous pivot point
             - The forces point in opposite directions due to stepping over the minima
-        2. If forces are have any negative overlap and the energy has increased.
-            - The minima has been stepped over but the forces are not exactly opposite
             due to finite step size
 
         At the time this function is called, the point/geometry is assumed to not have been added
@@ -298,7 +293,7 @@ class IRCHistory(object):
         optimization has been completed.
         """
 
-        unit_f = f_q / np.linalg.norm(f_q)  # current forces (pivot point)
+        unit_f = f_q / np.linalg.norm(f_q)  # current forces (at guess_point)
         f_rxn = self.f_q(step=-2)  # forces at most recent rxnpath point (q0)
         unit_f_rxn = f_rxn / np.linalg.norm(f_rxn)
         overlap = np.dot(unit_f, unit_f_rxn)
@@ -309,13 +304,11 @@ class IRCHistory(object):
 
         if overlap < irc_conv:
             return True
-        elif overlap < 0.0 and d_energy > 0.0:
+        if (abs(self.line_dist(step=-1) - self.line_dist(step=-2)) < self.step_size * 1e-3):
             return True
-
-        # TODO  Look at line distance criterion when distances are working.
-        # elif:
-        #    g_line_dist(p_irc_data->size()-1) - g_line_dist(p_irc_data->size()-2)) < s*10e-03)
-        #    return True
+        # AH Found to cause early termination in some test cases
+        # elif overlap < 0.0 and d_energy > 0.0 :
+        #     return True
 
         return False
 
@@ -429,19 +422,28 @@ class IRCHistory(object):
 
         """
 
-        logger.debug("Projecting out forces parallel to reaction path.")
-
         G_m = o_molsys.Gmat(massWeight=True)
-        G_m_inv = symm_mat_inv(G_m, redundant=True, small_val_limit=threshold)
+        G_m_inv = symm_mat_inv(G_m, redundant=True, threshold=threshold)
 
         q_vec = o_molsys.q_array()
         p_vec = q_vec - self.q_pivot()
-        logger.info(
-            "\ncurrent step from IRC pivot point (not previous point on rxnpath):\n %s", print_array_string(p_vec)
-        )
-        logger.info("\nForces at current point on hypersphere\n %s", print_array_string(f_q))
 
         G_m_inv_p = G_m_inv @ p_vec
         orthog_f = f_q - (f_q @ p_vec) / (p_vec @ G_m_inv_p) * G_m_inv_p
-        logger.debug("\nForces perpendicular to hypersphere.\n %s", print_array_string(orthog_f))
         return orthog_f
+
+    def recompute_all_internals(self, molsys: molsys.Molsys):
+        # make a independent copy of molecular system to change all internal coordinate values
+        # into the new internal basis set
+        tmp_molsys = copy.deepcopy(molsys)
+
+        # For each step update the irc_point coords, forces coords, and pivot points coords
+        for step in self.irc_points:
+            tmp_molsys.geom = step.x
+            step.q = tmp_molsys.q_array()
+            step.f_q = tmp_molsys.gradient_to_internals(step.f_x)
+
+            # Last point may not be fully set (could be none)
+            if isinstance(step.x_pivot, np.ndarray):
+                tmp_molsys.geom = step.x_pivot
+                step.q_pivot = tmp_molsys.q_array()

optking/addIntcos.py
@@ -43,7 +43,9 @@ def connectivity_from_distances(geom, Z, covalent_connect=1.3):
     C = np.zeros((len(geom), len(geom)), bool)
     for i, j in combinations(range(nat), 2):
         R = v3d.dist(geom[i], geom[j])
-        Rcov = qcel.covalentradii.get(Z[i], missing=4.0) + qcel.covalentradii.get(Z[j], missing=4.0)
+        Rcov = qcel.covalentradii.get(Z[i], missing=4.0) + qcel.covalentradii.get(
+            Z[j], missing=4.0
+        )
         # logger.debug("Checking atoms %d (Z=%d) and %d (Z=%d); R: %.3f; Rcov: %.3f; %s" %(i,
         #              Z[i],j,Z[j],R,Rcov, 'Y' if (R<op.Params.covalent_connect*Rcov) else 'N'))
         if R < covalent_connect * Rcov:
@@ -74,7 +76,7 @@ def add_auxiliary_bonds(connectivity, intcos, geom, Z):
     """
     radii = qcel.covalentradii  # these are in bohr
     Natom = len(geom)  # also in bohr
-    Nadded=0
+    Nadded = 0
 
     for a, b in combinations(range(Natom), 2):
         # No auxiliary bonds involving H atoms
@@ -90,28 +92,31 @@ def add_auxiliary_bonds(connectivity, intcos, geom, Z):
         omit = False
         # Omit auxiliary bonds between a and b, if a-c-b
         for c in range(Natom):
-            if c not in [a,b]:
+            if c not in [a, b]:
                 if connectivity[a][c] and connectivity[b][c]:
                     omit = True
                     break
-        if omit: continue
+        if omit:
+            continue
 
         # Omit auxiliary bonds between a and b, if a-c-d-b
         for c in range(Natom):
-            if c not in [a,b]:
+            if c not in [a, b]:
                 if connectivity[c][a]:
                     for d in range(Natom):
-                        if d not in [a,b,c]:
-                             if connectivity[d][c] and connectivity[d][b]:
-                                 omit = True
-                                 break
-                    if omit: break
-        if omit: continue
+                        if d not in [a, b, c]:
+                            if connectivity[d][c] and connectivity[d][b]:
+                                omit = True
+                                break
+                    if omit:
+                        break
+        if omit:
+            continue
 
         s = stre.Stre(a, b)
         if s not in intcos:
-            logger.info("Adding auxiliary bond %d - %d" % (a+1,b+1))
-            logger.info("Rcov = %10.5f; R = %10.5f; R/Rcov = %10.5f" % (Rcov, R, R/Rcov))
+            logger.info("Adding auxiliary bond %d - %d" % (a + 1, b + 1))
+            logger.info("Rcov = %10.5f; R = %10.5f; R/Rcov = %10.5f" % (Rcov, R, R / Rcov))
             intcos.append(s)
             Nadded += 1
 
@@ -281,7 +286,6 @@ def add_tors_from_connectivity(C, intcos, geom):
         if C[i, j]:
             for k in range(Natom):
                 if C[k, j] and k != i:
-
                     # ensure i-j-k is not collinear; that a regular such bend exists
                     b = bend.Bend(i, j, k)
                     if b not in intcos:
@@ -289,7 +293,6 @@ def add_tors_from_connectivity(C, intcos, geom):
 
                     for l in range(i + 1, Natom):
                         if C[l, k] and l != j:
-
                             # ensure j-k-l is not collinear
                             b = bend.Bend(j, k, l)
                             if b not in intcos:
@@ -315,7 +318,6 @@ def add_tors_from_connectivity(C, intcos, geom):
                     nbonds = sum(C[m])
 
                     if nbonds == 2:  # Nothing else is bonded to m
-
                         # look for an 'I' for I-J-[m]-k-L such that I-J-K is not collinear
                         J = j
                         i = 0
@@ -382,7 +384,7 @@ def add_oofp_from_connectivity(C, intcos, geom):
     for T in terminal_atoms:
         vertex_atoms.append(np.where(C[T])[0][0])
 
-    for (T, V) in zip(terminal_atoms, vertex_atoms):
+    for T, V in zip(terminal_atoms, vertex_atoms):
         if Nneighbors[V] < 3:
             pass
         # Find at least 2 other/side atoms
@@ -418,6 +420,7 @@ def add_oofp_from_connectivity(C, intcos, geom):
             if not covered:
                 try:
                     im_tors = improper_torsion_around_oofp(
+<<<<<<< HEAD
                         coord.atoms[1],
                         coord.atoms[0],
                         coord.atoms[2],
@@ -427,6 +430,16 @@ def add_oofp_from_connectivity(C, intcos, geom):
                 except AlgError:
                     raise AlgError("Tried to add out-of-plane angles but couldn't evaluate all of them.", oofp_failures=oofp.Oofp(T, V, side1, side2))
 
+=======
+                        coord.atoms[1], coord.atoms[0], coord.atoms[2], coord.atoms[3]
+                    )
+                    intcos.append(im_tors)
+                except AlgError:
+                    raise AlgError(
+                        "Tried to add out-of-plane angles but couldn't evaluate all of them.",
+                        oofp_failures=oofp.Oofp(T, V, side1, side2),
+                    )
+>>>>>>> 1b3497d (IRC bug fixes. Steps not being added to history, incorrect massweighting of first step, incorrect sign when scaling pivot point step, incorrect massweighting during hypersphere opt, allows linear_bend error handling by force updating IRC history, adds step length check from old c++ optking)
     return
 
 
@@ -479,7 +492,7 @@ def linear_bend_check(o_molsys):
                 A, B, C = intco.A, intco.B, intco.C
 
                 # <ABC < 0.  A-C-B should be linear bends.
-                if new_val < 0.0:
+                if new_val < np.pi - op.Params.linear_bend_threshold:
                     linear_bends.append(bend.Bend(A, C, B, bend_type="LINEAR"))
                     linear_bends.append(bend.Bend(A, C, B, bend_type="COMPLEMENT"))
 
@@ -489,10 +502,16 @@ def linear_bend_check(o_molsys):
                     linear_bends.append(bend.Bend(A, B, C, bend_type="COMPLEMENT"))
 
         missing_bends = [b for b in linear_bends if b not in frag.intcos]
-        bend_report = [f"{b}, already present.\n" if b not in missing_bends else f"{b}, missing.\n" for b in linear_bends]
+        bend_report = [
+            f"{b}, already present.\n" if b not in missing_bends else f"{b}, missing.\n"
+            for b in linear_bends
+        ]
 
         if missing_bends:
-            logger.warning("\n\tThe following linear bends should be present:\n %s", "\t".join(bend_report))
+            logger.warning(
+                "\n\tThe following linear bends should be present:\n %s",
+                "\t".join(bend_report),
+            )
         # Need to reset this or linear bends will be rechecked for alternate fragments
         linear_bends = []
 
@@ -664,7 +683,9 @@ def frozen_tors_from_input(frozen_tors_list, o_molsys):
         if len(T) != 4:
             raise OptError("Num. of atoms in frozen torsion should be 4.")
 
-        torsAngle = tors.Tors(T[0] - 1, T[1] - 1, T[2] - 1, T[3] - 1, constraint="frozen")
+        torsAngle = tors.Tors(
+            T[0] - 1, T[1] - 1, T[2] - 1, T[3] - 1, constraint="frozen"
+        )
         f = check_fragment(torsAngle.atoms, o_molsys)
         try:
             I = o_molsys.fragments[f].intcos.index(torsAngle)
@@ -739,7 +760,9 @@ def frozen_oofp_from_input(frozenOofpList, o_molsys):
         if len(T) != 4:
             raise OptError("Num. of atoms in frozen out-of-plane should be 4.")
 
-        oofpAngle = oofp.Oofp(T[0] - 1, T[1] - 1, T[2] - 1, T[3] - 1, constraint="frozen")
+        oofpAngle = oofp.Oofp(
+            T[0] - 1, T[1] - 1, T[2] - 1, T[3] - 1, constraint="frozen"
+        )
         f = check_fragment(oofpAngle.atoms, o_molsys)
         try:
             I = o_molsys.fragments[f].intcos.index(oofpAngle)
@@ -890,7 +913,9 @@ def check_fragment(atom_list, o_molsys):
     """
     fragList = o_molsys.atom_list2unique_frag_list(atom_list)
     if len(fragList) != 1:
-        logger.error("Coordinate contains atoms in different fragments. Not currently supported.\n")
+        logger.error(
+            "Coordinate contains atoms in different fragments. Not currently supported.\n"
+        )
         raise OptError("Atom list contains multiple fragments.")
     return fragList[0]
 
@@ -967,8 +992,9 @@ def add_constrained_intcos(o_molsys, params):
         torsions = frozen(params.unfreeze_dihedrals, 4)
         freeze_all_torsions(o_molsys, torsions)
 
+
 def freeze_all_torsions(molsys, skipped_torsions=[]):
-    """ Freeze all intrafragment torsions.
+    """Freeze all intrafragment torsions.
     Parameters
     ----------
     molsys: molsys.Molsys
@@ -989,11 +1015,12 @@ def freeze_all_torsions(molsys, skipped_torsions=[]):
 
         try:
             index = molsys.fragments[f].intcos.index(new_tors)
+            # TODO this looks like a clear error
             frag.intcos[index].unfreeze()
         except ValueError:
             logger.info(
                 "dihedral angle %s was unfrozen but was not present - adding it.",
-                new_tors
+                new_tors,
             )
             frag.intcos.append(new_tors)
 
@@ -1047,41 +1074,57 @@ def add_dimer_frag_intcos(o_molsys, params):
             # Find ref. pt. 2 on A.
             if not o_molsys.fragments[A].is_atom():
                 for i in range(o_molsys.fragments[A].natom):
-                    if i == refA1 or are_collinear(xyzA[i], xyzA[refA1], xyzB[refB1], col_tol):
+                    if i == refA1 or are_collinear(
+                        xyzA[i], xyzA[refA1], xyzB[refB1], col_tol
+                    ):
                         continue
                     refA2 = i
                     frag_ref_atomsA.append([refA2])
                     break
                 else:
-                    raise OptError("could not find 2nd atom on fragment {:d}".format(A + 1))
+                    raise OptError(
+                        "could not find 2nd atom on fragment {:d}".format(A + 1)
+                    )
             # Find ref. pt. 2 on B.
             if not o_molsys.fragments[B].is_atom():
                 for i in range(o_molsys.fragments[B].natom):
-                    if i == refB1 or are_collinear(xyzB[i], xyzB[refB1], xyzA[refA1], col_tol):
+                    if i == refB1 or are_collinear(
+                        xyzB[i], xyzB[refB1], xyzA[refA1], col_tol
+                    ):
                         continue
                     refB2 = i
                     frag_ref_atomsB.append([refB2])
                     break
                 else:
-                    raise OptError("could not find 2nd atom on fragment {:d}".format(B + 1))
+                    raise OptError(
+                        "could not find 2nd atom on fragment {:d}".format(B + 1)
+                    )
             # Find ref. pt. 3 on A.
-            if o_molsys.fragments[A].natom > 2 and not o_molsys.fragments[A].is_linear():
+            if (o_molsys.fragments[A].natom > 2 and not o_molsys.fragments[A].is_linear()):
                 for i in range(o_molsys.fragments[A].natom):
-                    if i in [refA1, refA2] or are_collinear(xyzA[i], xyzA[refA2], xyzA[refA1], col_tol):
+                    if i in [refA1, refA2] or are_collinear(
+                        xyzA[i], xyzA[refA2], xyzA[refA1], col_tol
+                    ):
                         continue
                     frag_ref_atomsA.append([i])
                     break
                 else:
-                    raise OptError("could not find 3rd atom on fragment {:d}".format(A + 1))
+                    raise OptError(
+                        "could not find 3rd atom on fragment {:d}".format(A + 1)
+                    )
             # Find ref. pt. 3 on B.
-            if o_molsys.fragments[B].natom > 2 and not o_molsys.fragments[B].is_linear():
+            if (o_molsys.fragments[B].natom > 2 and not o_molsys.fragments[B].is_linear()):
                 for i in range(o_molsys.fragments[B].natom):
-                    if i in [refB1, refB2] or are_collinear(xyzB[i], xyzB[refB2], xyzB[refB1], col_tol):
+                    if i in [refB1, refB2] or are_collinear(
+                        xyzB[i], xyzB[refB2], xyzB[refB1], col_tol
+                    ):
                         continue
                     frag_ref_atomsB.append([i])
                     break
                 else:
-                    raise OptError("could not find 3rd atom on fragment {:d}".format(A + 1))
+                    raise OptError(
+                        "could not find 3rd atom on fragment {:d}".format(A + 1)
+                    )
 
             df = dimerfrag.DimerFrag(A, frag_ref_atomsA, B, frag_ref_atomsB)
             df.update_reference_geometry(o_molsys.frag_geom(A), o_molsys.frag_geom(B))
@@ -1093,8 +1136,8 @@ def add_dimer_frag_intcos(o_molsys, params):
 
 
 def improper_torsion_around_oofp(center, a, b, c, geom):
-    """ To help compensate for missing the oofp. Create an improper torsion which goes from
-    T1-T2-C-T3 where T denotes terminal atoms, C denotes the OOFP center, and T1-C-T3 is linear """
+    """To help compensate for missing the oofp. Create an improper torsion which goes from
+    T1-T2-C-T3 where T denotes terminal atoms, C denotes the OOFP center, and T1-C-T3 is linear"""
 
     if v3d.are_collinear(geom[center], geom[a], geom[b]):
         return tors.Tors(a, c, center, b)

optking/displace.py
@@ -29,19 +29,14 @@ logger = logging.getLogger(f"{log_name}{__name__}")
 
 
 # Displace molecular system
-def displace_molsys(
-        molsys: Molsys,
-        dq_in,
-        fq=None,
-        **kwargs
-    ):
+def displace_molsys(molsys: Molsys, dq_in, fq=None, **kwargs):
     """Manage internal coordinate step for a molecular system
 
     Parameters
     ----------
     oMolsys : Molsys
         input molecular system
-    dq : np.ndarray 
+    dq : np.ndarray
         input coordinatestep
     fq : np.ndarray
         forces in internal coordinates (used for printing). passed in au. converted to aJ
@@ -53,7 +48,7 @@ def displace_molsys(
         [1, 2, 3, 4, 5] How much output to show. May require DEBUG for logging
     opt_type : str (optional)
         default MIN. IRC requires convergence
-    small_val_limit : float (optional)
+    threshold : float (optional)
         threshold for singular values to not invert
     bt_dx_conv : float (optional)
         default : 1.0e-12 how tightly to converge cartesian coordinates in backtransformation
@@ -100,18 +95,19 @@ def displace_molsys(
         if frag.frozen or frag.num_intcos == 0:
             continue
         logger.info("\tDetermining Cartesian step for fragment %d." % (f + 1))
-        dq_frag, conv = displace_frag(
-            frag,
-            dq_in[molsys.frag_intco_slice(f)],
-            **kwargs
-        )
+        dq_frag, conv = displace_frag(frag, dq_in[molsys.frag_intco_slice(f)], **kwargs)
 
     for i, DI in enumerate(molsys.dimer_intcos):
-        logger.info("\tTaking step for dimer coordinates of fragments %d and %d." % (DI.A_idx + 1, DI.B_idx + 1))
+        logger.info(
+            "\tTaking step for dimer coordinates of fragments %d and %d."
+            % (DI.A_idx + 1, DI.B_idx + 1)
+        )
 
         axyz = molsys.frag_geom(DI.A_idx)
         bxyz = molsys.frag_geom(DI.B_idx)
-        bxyz[:] = DI.orient_fragment(axyz, bxyz, q_target[molsys.dimerfrag_intco_slice(i)])
+        bxyz[:] = DI.orient_fragment(
+            axyz, bxyz, q_target[molsys.dimerfrag_intco_slice(i)]
+        )
 
     geom_final = molsys.geom
     # Analyze relative to original input geometry
@@ -132,12 +128,20 @@ def displace_molsys(
 
     intco_lbls = molsys.intco_lbls
 
-    coordinate_change_report = "\n\n\t        --- Internal Coordinate Step in ANG or DEG, aJ/ANG or AJ/DEG ---\n"
-    coordinate_change_report += "\t-------------------------------------------------------------------------------\n"
+    coordinate_change_report = (
+        "\n\n\t        --- Internal Coordinate Step in ANG or DEG, aJ/ANG or AJ/DEG ---\n"
+    )
+    coordinate_change_report += (
+        "\t-------------------------------------------------------------------------------\n"
+    )
 
     if fq is None:
-        coordinate_change_report += "\t           Coordinate      Previous         Change          New \n"
-        coordinate_change_report += "\t           ----------      --------        ------        ------\n"
+        coordinate_change_report += (
+            "\t           Coordinate      Previous         Change          New \n"
+        )
+        coordinate_change_report += (
+            "\t           ----------      --------        ------        ------\n"
+        )
         for i in range(len(dq_in)):
             coordinate_change_report += "\t%21s%14.5f%14.5f%14.5f\n" % (
                 intco_lbls[i],
@@ -161,7 +165,9 @@ def displace_molsys(
                 dqShow[i],
                 qShow_final[i],
             )
-    coordinate_change_report += "\t-------------------------------------------------------------------------------\n"
+    coordinate_change_report += (
+        "\t-------------------------------------------------------------------------------\n"
+    )
     logger.info(coordinate_change_report)
 
     # Return final, total displacement ACHIEVED
@@ -193,7 +199,7 @@ def displace_frag(frag, dq_in, **kwargs):
         [1, 2, 3, 4, 5] How much output to show. May require DEBUG for logging
     opt_type : str (optional)
         default MIN. IRC requires convergence
-    small_val_limit : float (optional)
+    threshold : float (optional)
         threshold for singular values to not invert
     bt_dx_conv : float (optional)
         default : 1.0e-12 how tightly to converge cartesian coordinates in backtransformation
@@ -209,10 +215,9 @@ def displace_frag(frag, dq_in, **kwargs):
     """
 
     ensure_convergence = kwargs.get(
-        "ensure_convergence",
-        kwargs.get("ensure_bt_convergence", False)
+        "ensure_convergence", kwargs.get("ensure_bt_convergence", False)
     )
-    
+
     geom = frag.geom
     dq = dq_in.copy()
     if not frag.num_intcos or not len(geom) or not len(dq_in):
@@ -246,10 +251,14 @@ def displace_frag(frag, dq_in, **kwargs):
                     )
                     break
                 else:
-                    geom[:] = geom_orig  # put original geometry back for next try at smaller step.
+                    geom[:] = (
+                        geom_orig  # put original geometry back for next try at smaller step.
+                    )
 
         if conv and cnt > 0:  # We were able to take a modest step.  Try to complete it.
-            logger.info("\tAble to take a small step; trying another partial back-transformations.\n")
+            logger.info(
+                "\tAble to take a small step; trying another partial back-transformations.\n"
+            )
 
             for j in range(1, 2 * cnt):
                 logger.info("\tMini-step %d of %d.\n", j + 1, 2 * cnt)
@@ -262,7 +271,9 @@ def displace_frag(frag, dq_in, **kwargs):
                 frag.unfix_bend_axes()
 
                 if not conv:
-                    logger.warning("\tCouldn't converge this mini-step; quitting with previous geometry.\n")
+                    logger.warning(
+                        "\tCouldn't converge this mini-step; quitting with previous geometry.\n"
+                    )
                     geom[:] = best_geom
                     break
 
@@ -272,13 +283,14 @@ def displace_frag(frag, dq_in, **kwargs):
         conv = back_transformation(frag.intcos, geom, dq, **kwargs)
         frag.unfix_bend_axes()
 
-        if kwargs.get("opt_type", "MIN") == "IRC" and not conv:
-            raise OptError("Could not take constrained step in an IRC computation.")
+        # if kwargs.get("opt_type", "MIN") == "IRC" and not conv:
+        #     raise OptError("Could not take constrained step in an IRC computation.")
 
     # Fix drift/error in any frozen coordinates
     frozen_conv = True
-    if any(intco.frozen for intco in frag.intcos) or any(intco.ranged for intco in frag.intcos):
-
+    if any(intco.frozen for intco in frag.intcos) or any(
+        intco.ranged for intco in frag.intcos
+    ):
         frag.update_dihedral_orientations()
         frag.fix_bend_axes()
         qnow = intcosMisc.q_values(frag.intcos, geom)
@@ -299,10 +311,12 @@ def displace_frag(frag, dq_in, **kwargs):
 
         # Could be streamlined with above, but for now testing to see if helps
         constrained_intcos = list(compress(frag.intcos, constrained_coord_selector))
-        constrained_dq = np.asarray(list(compress(dq_adjust_frozen, constrained_coord_selector)))
+        constrained_dq = np.asarray(
+            list(compress(dq_adjust_frozen, constrained_coord_selector))
+        )
         adjust_info = "\nAdjustments to Frozen/Ranged Coordinates Needed:\n"
-        for l,v in zip([str(i) for i in constrained_intcos], constrained_dq):
-            adjust_info += f'{l:>8s}{v:10.6f}\n'
+        for l, v in zip([str(i) for i in constrained_intcos], constrained_dq):
+            adjust_info += f"{l:>8s}{v:10.6f}\n"
         logger.info(adjust_info)
 
         # For stability try scaling the adjustment if its quite long.
@@ -311,16 +325,18 @@ def displace_frag(frag, dq_in, **kwargs):
             scale = 0.5 / np.linalg.norm(dq_adjust_frozen)
             dq_adjust_frozen *= scale
 
-        frozen_msg = "\tAdditional back-transformation to adjust frozen/ranged coordinates: "
+        frozen_msg = (
+            "\tAdditional back-transformation to adjust frozen/ranged coordinates: "
+        )
 
         # suppress printing for the next stage
         kwargs.update({"print_lvl": kwargs.get("print_lvl", 1) - 1})
         frozen_conv = back_transformation(
-            constrained_intcos, #F.intcos,
+            constrained_intcos,  # F.intcos,
             geom,
-            constrained_dq, # dq_adjust_frozen,
-            **kwargs
-            )
+            constrained_dq,  # dq_adjust_frozen,
+            **kwargs,
+        )
 
         # unsuppress printing for the next stage
         kwargs.update({"print_lvl": kwargs.get("print_lvl", 0) + 1})
@@ -356,13 +372,7 @@ def displace_frag(frag, dq_in, **kwargs):
     return dq, conv and frozen_conv
 
 
-def back_transformation(
-    intcos,
-    geom,
-    dq,
-    **kwargs
-):
-
+def back_transformation(intcos, geom, dq, **kwargs):
     print_lvl = kwargs.get("print_lvl", 1)
     bt_dx_conv = kwargs.get("bt_dx_conv", 1.0e-12)
     bt_dx_rms_change_conv = kwargs.get("bt_dx_rms_change_conv", 1.0e-12)
@@ -374,7 +384,7 @@ def back_transformation(
     q_orig = intcosMisc.q_values(intcos, geom)
     q_target = q_orig + dq
 
-    if print_lvl > 1: # printing is supressed for frozen coordinate cleanup
+    if print_lvl > 1:  # printing is supressed for frozen coordinate cleanup
         target_step_str = "Initial target in back_transformation():\n"
         target_step_str += "          Original         Target           Dq\n"
         for i in range(len(dq)):
@@ -400,7 +410,6 @@ def back_transformation(
     bt_iter_cnt = 0
 
     while bt_iter_continue:
-
         # dq_rms = rms(dq)
         dx_rms, dx_max = dq_to_dx(intcos, geom, dq, **kwargs)
 
@@ -409,7 +418,11 @@ def back_transformation(
             bt_converged = True
             bt_iter_continue = False
         # No further progress toward convergence.
-        elif np.absolute(dx_rms - dx_rms_last) < bt_dx_rms_change_conv or bt_iter_cnt >= bt_max_iter or dx_rms > 100.0:
+        elif (
+            np.absolute(dx_rms - dx_rms_last) < bt_dx_rms_change_conv
+            or bt_iter_cnt >= bt_max_iter
+            or dx_rms > 100.0
+        ):
             bt_converged = False
             bt_iter_continue = False
 
@@ -433,12 +446,14 @@ def back_transformation(
             )
         bt_iter_cnt += 1
 
-    if print_lvl > 0:
-        step_iter_str += "\t---------------------------------------------------\n"
-        logger.debug(step_iter_str)
-
-    bt_final_step = f"\tRMS(dx): {dx_rms: .3e} \tMax(dx): {dx_max: .3e} \tRMS(dq): {dq_rms: .3e}"
+    bt_final_step = (
+        f"\tRMS(dx): {dx_rms: .3e} \tMax(dx): {dx_max: .3e} \tRMS(dq): {dq_rms: .3e}"
+    )
     if bt_converged:
+        if print_lvl > 0:
+            step_iter_str += "\t---------------------------------------------------\n"
+            logger.debug(step_iter_str)
+
         logger.info("\tSuccessfully converged to displaced geometry.")
         logger.info(bt_final_step)
     else:
@@ -446,7 +461,13 @@ def back_transformation(
         logger.warning(bt_final_step)
 
     if dq_rms > best_dq_rms:
-        logger.warning("\tPrevious geometry is closer to target in internal coordinates, so using that one.\n")
+        if print_lvl > 0:
+            step_iter_str += "\t---------------------------------------------------\n"
+            logger.debug(step_iter_str)
+
+        logger.warning(
+            "\tPrevious geometry is closer to target in internal coordinates, so using that one.\n"
+        )
         logger.warning("\tBest geometry has RMS(Delta(q)) = %8.2e\n" % best_dq_rms)
         geom[:] = best_geom
 
@@ -470,7 +491,7 @@ def dq_to_dx(intcos, geom, dq, **kwargs):
     dq : displacement in internal coordinates
     print_details : bool
         whether to print the attempted and achieved dq
-    small_val_limit : float
+    threshold : float
         tolerance for inversion of singular values. This argument corresponds to rcond in
         numpy.linalg.pinv()
 
@@ -483,11 +504,11 @@ def dq_to_dx(intcos, geom, dq, **kwargs):
     """
 
     print_details = kwargs.get("print_details", False)
-    small_val_limit = kwargs.get("small_val_limit", 1e-6)
+    threshold = kwargs.get("threshold", 1e-10)
 
     B = intcosMisc.Bmat(intcos, geom)
     G = B @ B.T
-    Ginv = symm_mat_inv(G, redundant=True, small_val_limit=small_val_limit)
+    Ginv = symm_mat_inv(G, redundant=True, threshold=threshold)
     dx = B.T @ Ginv @ dq
 
     # If the step in cartesian coordinates is irregularly large,
@@ -496,20 +517,13 @@ def dq_to_dx(intcos, geom, dq, **kwargs):
     dq_len = np.linalg.norm(dq)
     dx_len = np.linalg.norm(dx)
     if dx_len > 10 * dq_len:
-
         logger.debug(
             "Cartesian step is %f times larger than internal coordinate step",
-            dx_len / dq_len
+            dx_len / dq_len,
         )
 
-        eigvals = np.linalg.eigvalsh(G)
-        largest = np.max(eigvals)
-        threshold = largest * small_val_limit
-        smallest = np.min(eigvals[eigvals > threshold])
-        new_threshold = (smallest/largest + 1e-10)
-
         # recompute step
-        Ginv = symm_mat_inv(G, redundant = True, small_val_limit=new_threshold)
+        Ginv = symm_mat_inv(G, redundant=True, threshold=threshold / 100)
         dx = B.T @ Ginv @ dq
 
         if np.linalg.norm(dx) > 10 * dq_len:

optking/linearAlgebra.py
@@ -104,7 +104,7 @@ def asymm_mat_eig(mat):
     return evals.real, evects.real.T
 
 
-def symm_mat_inv(A, redundant=False, small_val_limit=1.0e-10):
+def symm_mat_inv(A, redundant=False, threshold=1.0e-10):
     """
     Return the inverse of a real, symmetric matrix.
 
@@ -113,7 +113,7 @@ def symm_mat_inv(A, redundant=False, small_val_limit=1.0e-10):
     A : np.ndarray
     redundant : bool
         allow generalized inverse
-    smallValLimit : float
+    threshold : float
         specifies how small of singular values to invert
 
     Returns
@@ -128,7 +128,6 @@ def symm_mat_inv(A, redundant=False, small_val_limit=1.0e-10):
 
     try:
         if redundant:
-
             if logger.isEnabledFor(logging.DEBUG):
                 try:
                     evals, evects = np.linalg.eigh(A)
@@ -136,12 +135,22 @@ def symm_mat_inv(A, redundant=False, small_val_limit=1.0e-10):
                     raise OptError("symm_mat_inv: could not compute eigenvectors")
 
                 absEvals = np.abs(evals)
-                threshold = small_val_limit * np.max(absEvals)
-                logger.debug("Singular | values | > %8.3e will be inverted." % threshold)
-                val = np.min(absEvals[absEvals > threshold])
-                logger.debug("Smallest inverted value is %8.3e." % val)
 
-            return np.linalg.pinv(A, rcond=small_val_limit)
+                # numpy uses a relative size comparison anything less than rcond * largest val
+                # is zeroed out. Therefore larger values of rcond tighten the critera.
+                # We want a `threshold` (params.linear_algebra_tol) to tighten the criteria
+                # compute rcond such that any eigenvalues smaller than threshold are zeroed
+                rcond = threshold / np.max(absEvals)
+
+                # logger.debug("Singular | values | > %8.3e will be inverted." % threshold)
+                val = np.min(absEvals[absEvals > threshold])
+                if val < 1e-8:
+                    logger.warning(
+                        "Inverting a small eigenvalue. System may include redundancies"
+                    )
+                    logger.warning("Smallest inverted value is %8.3e." % val)
+
+            return np.linalg.pinv(A, rcond)
 
         else:
             return np.linalg.inv(A)

optking/optimize.py
@@ -2,7 +2,6 @@
 looking to add features to the code to familarize themselves with the overall workings of OptKing.
 Functions may be useful to users seeking greater control over the inner workings of OptKing than provided by the
 ``OptHelpers``. For instance, if manually creating a molecular system or manually controlling / switching algorithms
-on the fly.
 
 See Also
 --------
@@ -101,7 +100,13 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
 
     _LINESEARCHES = {"ENERGY": linesearch.ThreePointEnergy}
 
-    def __init__(self, molsys: Molsys, history_object: history.History, params: op.OptParams, computer: ComputeWrapper):
+    def __init__(
+        self,
+        molsys: Molsys,
+        history_object: history.History,
+        params: op.OptParams,
+        computer: ComputeWrapper,
+    ):
         super().__init__(molsys, history_object, params)
         self.direction: Union[np.ndarray, None] = None
 
@@ -113,7 +118,9 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         self.stashed_hessian = None
 
         if params.linesearch:
-            self.linesearch_method = OptimizationManager._LINESEARCHES["ENERGY"](molsys, self.history, params)
+            self.linesearch_method = OptimizationManager._LINESEARCHES["ENERGY"](
+                molsys, self.history, params
+            )
             self.opt_method.trust_radius_on = False
         self.requires = self.update_requirements()
         self.current_requirements = self.update_requirements()
@@ -121,7 +128,10 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         self.erase_hessian = False
         self.check_linesearch = True
         self.error = None
-        self.protocol = {"protocol": None, "step_number": -100} # Just a number that will never occur naturally
+        self.protocol = {
+            "protocol": None,
+            "step_number": -100,
+        }  # Just a number that will never occur naturally
 
     def to_dict(self):
         """Convert attributes to serializable form."""
@@ -171,9 +181,9 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
             )  # Can just recreate with current history and params
 
         if d.get("linesearch_method"):
-            manager.linesearch_method = OptimizationManager._LINESEARCHES["ENERGY"].from_dict(
-                d["linesearch_method"], molsys, history, params
-            )
+            manager.linesearch_method = OptimizationManager._LINESEARCHES[
+                "ENERGY"
+            ].from_dict(d["linesearch_method"], molsys, history, params)
 
         return manager
 
@@ -212,7 +222,15 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         requirements = self.opt_method.requires()
         hessian_protocol = self.get_hessian_protocol(self.step_number)
         protocol = hessian_protocol["protocol"]
-        H, g_q, g_x, E = get_pes_info(H, self.computer, self.molsys, self.history, self.params, protocol, requirements)
+        H, g_q, g_x, E = get_pes_info(
+            H,
+            self.computer,
+            self.molsys,
+            self.history,
+            self.params,
+            protocol,
+            requirements,
+        )
 
         logger.info("%s", print_geom_grad(self.molsys.geom, g_x))
 
@@ -252,15 +270,24 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
                 self.linesearch_method.start(self.direction)
 
                 if self.check_linesearch:
-                    self.history.append(self.molsys.geom, energy, fq, self.molsys.gradient_to_cartesians(-1 * fq))
+                    self.history.append(
+                        self.molsys.geom,
+                        energy,
+                        fq,
+                        self.molsys.gradient_to_cartesians(-1 * fq),
+                    )
                     ls_energy = self.linesearch_method.expected_energy
-                    dq_norm, unit_dq, grad, hess = self.opt_method.step_metrics(self.direction, fq, H)
+                    dq_norm, unit_dq, grad, hess = self.opt_method.step_metrics(
+                        self.direction, fq, H
+                    )
                     self.history.append_record(ls_energy, self.direction, unit_dq, grad, hess)
 
                 self.stashed_hessian = H
                 self.check_linesearch = False
 
-            achieved_dq, returned_str = self.linesearch_method.take_step(fq, H, energy, return_str=True)
+            achieved_dq, returned_str = self.linesearch_method.take_step(
+                fq, H, energy, return_str=True
+            )
 
             if self.linesearch_method.minimized:
                 logger.info("Linesearch complete. Next step will compute a new direction")
@@ -314,10 +341,13 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         # Hard quit if too many total steps taken (inc. all IRC points and algorithms).
         if iterations >= self.params.geom_maxiter:
             logger.error(
-                "\tTotal number of steps (%d) exceeds maximum allowed (%d).\n" % (iterations, self.params.geom_maxiter)
+                "\tTotal number of steps (%d) exceeds maximum allowed (%d).\n"
+                % (iterations, self.params.geom_maxiter)
             )
             raise OptError(
-                "Maximum number of steps exceeded: {}.".format(self.params.geom_maxiter),
+                "Maximum number of steps exceeded: {}.".format(
+                    self.params.geom_maxiter
+                ),
                 "OptError",
             )
 
@@ -349,7 +379,7 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         if self.erase_hessian is True:
             self.erase_hessian = False
             action = "compute" if self.params.full_hess_every > 0 else "update"
-            self.protocol.update({"protocol": action}) 
+            self.protocol.update({"protocol": action})
             return self.protocol
 
         if self.params.cart_hess_read:
@@ -413,7 +443,8 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
 
             affected_frags = []
             for bend in bends:
-                if bend.bend_type != "COMPLEMENT":  # no need to repeat this code for "COMPLEMENT"
+                # no need to repeat this code for "COMPLEMENT". Bend already removed for LINEAR
+                if (bend.bend_type != "COMPLEMENT"):
                     iF = addIntcos.check_fragment(bend.atoms, self.molsys)
                     F = self.molsys.fragments[iF]
                     affected_frags.append(F)
@@ -426,14 +457,19 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
 
             # Convert the Hessian back into the new coordinate system
             self.H = self.molsys.hessian_to_internals(Hx, gx)
-
         elif self.params.dynamic_level == self.params.dynamic_lvl_max:
-            logger.critical("\n\t Current algorithm/dynamic_level is %d.\n" % self.params.dynamic_level)
+            logger.critical(
+                "\n\t Current algorithm/dynamic_level is %d.\n"
+                % self.params.dynamic_level
+            )
             logger.critical("\n\t Alternative approaches are not available or turned on.\n")
             raise OptError("Maximum dynamic_level reached.")
         else:
             self.params.dynamic_level += 1
-            logger.warning("\n\t Increasing dynamic_level algorithm to %d.\n" % self.params.dynamic_level)
+            logger.warning(
+                "\n\t Increasing dynamic_level algorithm to %d.\n"
+                % self.params.dynamic_level
+            )
             logger.warning("\n\t Erasing old history, hessian, intcos.\n")
             eraseIntcos = True
             eraseHistory = True
@@ -452,8 +488,11 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
             self.clear()
             self.erase_hessian = True
 
+            if isinstance(self.opt_method, IRCfollowing.IntrinsicReactionCoordinate):
+                self.opt_method.irc_history.recompute_all_internals(self.molsys)
+
         self.error = "AlgError"
-    
+
     def opt_error_handler(self, error):
         """OptError indicates an unrecoverable error. Print information and trigger cleanup."""
         logger.critical("\tA critical optimization-specific error has occured.")
@@ -473,8 +512,10 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         return self._exception_cleanup(error)
 
     def _exception_cleanup(self, error):
-
-        logger.info("\tDumping history: Warning last point not converged.\n" + self.history.summary_string())
+        logger.info(
+            "\tDumping history: Warning last point not converged.\n %s",
+            self.history.summary_string()
+        )
 
         if self.params.opt_type == "IRC":
             logging.debug("\tDumping IRC points completed")
@@ -484,7 +525,6 @@ class OptimizationManager(stepAlgorithms.OptimizationInterface):
         return self.finish(error)
 
     def finish(self, error=None):
-
         rxnpath = None
         if self.params.opt_type == "IRC":
             self.opt_method.irc_history.progress_report()
@@ -556,7 +596,7 @@ def get_pes_info(
     Notes
     -----
     Some functionality from start_step has now been placed here. external forces are added into the forces.
-    Redundancies and constraints are projected out of the forces and hessian. 
+    Redundancies and constraints are projected out of the forces and hessian.
 
     """
 
@@ -723,7 +763,7 @@ def make_internal_coords(o_molsys: Molsys, params: op.OptParams):
     return
 
 
-def prepare_opt_output(o_molsys, computer, rxnpath=False, error=None):
+def prepare_opt_output(o_molsys, computer, rxnpath=[], error=None):
     logger.info("Preparing OptimizationResult")
     # Get molecule from most recent step. Add provenance and fill in non-required fills.
     # Turn back to dict
@@ -740,11 +780,28 @@ def prepare_opt_output(o_molsys, computer, rxnpath=False, error=None):
     }
 
     if error:
-        qc_output.update({"success": False, "error": {"error_type": type(error), "error_message": str(error)}})
+        qc_output.update(
+            {
+                "success": False,
+                "error": {"error_type": error.err_type, "error_message": error.mesg},
+            }
+        )
 
     if rxnpath:
         qc_output["extras"]["irc_rxn_path"] = rxnpath
-        qc_output["final_geometry"] = rxnpath[-2]["x"]
-        qc_output["extras"]["final_irc_energy"] = rxnpath[-2]["energy"]
+        if rxnpath[-2]:
+            qc_output["final_geometry"] = rxnpath[-2]["x"]
+            qc_output["extras"]["final_irc_energy"] = rxnpath[-2]["energy"]
+        else:
+            qc_output.update({
+                "success": False,
+                "error": {
+                    "error_type": "OptError",
+                    "error": """IRC calculation has terminated before taking a single step. Please
+                        ensure that the starting point is a transition state at the level of theory
+                        being utilized. If issues continue, decrease ``irc_convergence`` and/or
+                        ``irc_step_size``"""}
+                }
+            )
 
     return qc_output