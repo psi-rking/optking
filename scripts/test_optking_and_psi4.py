894c624c6afa3e858d45595dbcce63955826429f
Alexander Heide <agh66737@uga.edu>
2026-05-06 13:22:08 -0400

    Add publish workflow and use hatch in CI

--- scripts/test_optking_and_psi4.py
+++ scripts/test_optking_and_psi4.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # Super simple script for running tests via scratch
-p1 = subprocess.Popen(["pytest", "-rv", "--check_iter=1", "--show-capture=stderr"] + sys.argv[1:], env=os.environ)
+p1 = subprocess.Popen(["pytest", "-rv", "--check_iter=1", "--show-capture=stderr", "--cov=optking", "--color=yes", "--cov-report=xml", "optking/"] + sys.argv[1:], env=os.environ)
 p1_ec = p1.wait()
 p2 = subprocess.Popen(["psi4", "--test", "-m", "opt"], env=os.environ)
 p2_ec = p2.wait()
